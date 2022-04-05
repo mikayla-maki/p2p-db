ab3ae23b54a23e956fc6803813eeb30392de27ea
Mikayla Maki <mikayla.c.maki@icloud.com>
2022-04-04 23:40:52 -0700

    It begins

--- src/log.rs
+++ src/log.rs
@@ -1,13 +1,33 @@
-//Implementation inspired by studying the kafka impl: 
-// https://jaceklaskowski.gitbooks.io/apache-kafka/content/kafka-log-Log.html
+//Implementation inspired by studying the kafka tutorials on confluent: 
+//https://www.confluent.io/blog/kafka-streams-tables-part-1-event-streaming/
 
-use bytes::{Bytes, BytesMut, Buf, BufMut};
+use bytes::Bytes;
+use std::path::Path;
 
 struct LogSegment {
-    logicalTimestamp: u64,
+    logical_timestamp: u64,
+    mesage: Message
+}
+
+struct Message {
+    key: Bytes,
     msg: Bytes
 }
 
 struct Log {
+    cur_timestamp: u64,
     contents: Vec<LogSegment> //Concurrent-ize this? 
+}
+
+impl Log {
+    fn new() -> Log {
+        Log {
+            cur_timestamp: 0,
+            contents: vec![]
+        }
+    }
+
+    fn subscribe(start: usize) -> {
+
+    }
 }
